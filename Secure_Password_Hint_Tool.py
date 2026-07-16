35aaa40f57acadbc886dc5e6f5b25981c4a348cb
Thuto <ThutoSiya@gmail.com>
2026-07-16 21:47:51 +0200

    Clean password input and extract hint characters

--- Secure_Password_Hint_Tool.py
+++ Secure_Password_Hint_Tool.py
@@ -2,3 +2,9 @@
 
 # Ask the user to enter their password
 password = input("Enter your password: ")
+
+# Use .strip() to clean up any accidental spaces they might have typed at the start or end
+password = password.strip()
+
+first_letter = password[0]
+last_letter = password[-1]
